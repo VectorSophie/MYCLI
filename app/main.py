be06cfeb0fbcc87e5fe9f942c674e0e0b3ecdb98
VectorSophie <jay7math@gmail.com>
2025-05-28 16:41:31 +0900

    im so dumb lol

--- app/main.py
+++ app/main.py
@@ -5,19 +5,17 @@ def main():
     while True:
         sys.stdout.write("$ ")
 
-    enter = input()
-    commarr=enter.split()
+        enter = input()
+        commarr=enter.split()
 
-    command = commarr[0]
-    args = commarr[1:]
-    if command == "exit":
-        sys.exit(0)
-        continue
-    elif command == "echo":
-        print(" ".join(args))
-        continue
-    else:
-        print(f"{command}: command not found")
+        command = commarr[0]
+        args = commarr[1:]
+        if command == "exit":
+            sys.exit(0)
+        elif command == "echo":
+            print(" ".join(args))
+        else:
+            print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
