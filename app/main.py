4b67a24784a98631ce268c31614bc7b00ad7df2d
VectorSophie <jay7math@gmail.com>
2025-05-28 16:47:27 +0900

    sixth? i tend to forget

--- app/main.py
+++ app/main.py
@@ -1,19 +1,33 @@
 import sys
 
+def __echo__(args):
+    print(" ".join(args))
+
+def __type__(args, commands):
+    if args and args[0] in commands:
+        print(f"{args[0]} is a shell builtin")
+    else:
+        print(f"{args[0]}: not found")
 
 def main():
+    commands = {
+        "exit": lambda args: sys.exit(0),
+        "echo": __echo__,
+        "type": lambda args: __type__(args, commands)
+    }
+
     while True:
         sys.stdout.write("$ ")
-
         enter = input()
-        commarr=enter.split()
+        if not enter.strip():
+            continue
 
-        command = commarr[0]
-        args = commarr[1:]
-        if command == "exit":
-            sys.exit(0)
-        elif command == "echo":
-            print(" ".join(args))
+        commarr = enter.split()
+        command, args = commarr[0], commarr[1:]
+
+        func = commands.get(command)
+        if func:
+            func(args)
         else:
             print(f"{command}: command not found")
 
